36ccb6a2e0448c228dbd8f6d90d76ca1ac0c9c9a
Nikola Pavicevic <nijepa@gmail.com>
2022-03-17 18:18:44 +0100

    vue config fix

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/f1masters-frontend/" : "/",
+    process.env.NODE_ENV === "production" ? "/f1master-frontend/" : "/",
 };
